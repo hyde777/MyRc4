1b6b187d3eff6f40fae60d1f028be6563a198760
Loghan <loghan.r@gmail.com>
2018-12-06 18:32:45 +0100

    \!/ Ne compile pas ! En cours de parallélisme

--- MyRc4/MyRc4.h
+++ MyRc4/MyRc4.h
@@ -6,24 +6,24 @@
 * RC4 Encryptor utility for decrypting Strings
 * @brief Utility to RC4 encrypt bytes
 */
+
 class MyRc4 {
+
 public:
+	void setKey(unsigned char k[], int size);
+	void encrypt(unsigned char* in, unsigned char* out, int begin, int ending);
+	void encrypt(char* in, char* out, int begin, int ending);
 
-	void setKey(unsigned char * k, int size);
-
-	void encrypt(unsigned char * in, unsigned char * out, int size);
-	
-	void encrypt(char * in, char * out, int size);
 	MyRc4();
+
 protected:
 	void ksa(unsigned char * key);
 	void swap(unsigned char data[], int i, int j);
-	void prga(unsigned char * plaintext, unsigned char * cipher, int size);
-	void prga(char * plaintext, char * cipher, int size);
+	void prga(unsigned char * plaintext, unsigned char * cipher, int begin, int ending);
+	void prga(char * plaintext, char * cipher, int begin, int ending);
+
 	unsigned char sbox[256];
 	int sizeKey, prgaIndexA, prgaIndexB;
 };
-
-
 
 #endif /* ARC4_H_ */
