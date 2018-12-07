5449bd15342ede6d2f8187f940f92dcbe55f1708
Loghan <loghan.r@gmail.com>
2018-12-07 13:33:51 +0100

    Refacto of file structure tree + CMakeLists.txt

--- MyRc4/src/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/C++/MyRc4/MyRc4/src/benchmark
-# Build directory: D:/Project/C++/MyRc4/MyRc4/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("googletest")
-subdirs("test")
--- MyRc4/src/build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/C++/MyRc4/MyRc4/src/benchmark/googletest
-# Build directory: D:/Project/C++/MyRc4/MyRc4/src/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
