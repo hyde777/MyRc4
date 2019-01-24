737cef09deb9ebed6445cfc1a24d0d4b593ca8d9
Loghan <loghan.r@gmail.com>
2019-01-25 00:29:16 +0100

    Reorganize argument

--- src/main.cpp
+++ src/main.cpp
@@ -18,11 +18,13 @@
 
 using namespace std;
 
-MyRc4::MyRc4() {}
-
-void MyRc4::prga(unsigned char* plaintext, unsigned char* cipher, int begin, int ending) 
+MyRc4::MyRc4()
 {
-	for (int k = begin; k < ending; k++) 
+}
+
+void MyRc4::prga(unsigned char* plaintext, unsigned char* cipher, int begin, int ending)
+{
+	for (int k = begin; k < ending; k++)
 	{
 		prgaIndexA = (prgaIndexA + 1) % 256;
 		prgaIndexB = (prgaIndexB + sbox[prgaIndexA]) % 256;
@@ -31,39 +33,39 @@ void MyRc4::prga(unsigned char* plaintext, unsigned char* cipher, int begin, int
 	}
 }
 
-void MyRc4::prga(char* plaintext, char* cipher, int begin, int ending) 
+void MyRc4::prga(char* plaintext, char* cipher, int begin, int ending)
 {
 	prga(reinterpret_cast<unsigned char *>(plaintext), reinterpret_cast<unsigned char*>(cipher), begin, ending);
 }
 
-void MyRc4::encrypt(char* plaintext, char* ciphertext, int begin, int ending) 
+void MyRc4::encrypt(char* plaintext, char* ciphertext, int begin, int ending)
 {
 	prga(plaintext, ciphertext, begin, ending);
 }
 
-void MyRc4::encrypt(unsigned char* plaintext, unsigned char * ciphertext, int begin, int ending) 
+void MyRc4::encrypt(unsigned char* plaintext, unsigned char* ciphertext, int begin, int ending)
 {
 	prga(plaintext, ciphertext, begin, ending);
 }
 
-void MyRc4::swap(unsigned char data[], int i, int j) 
+void MyRc4::swap(unsigned char data[], int i, int j)
 {
 	unsigned char temp = data[i];
 	data[i] = data[j];
 	data[j] = temp;
 }
 
-void MyRc4::ksa(unsigned char* key) 
+void MyRc4::ksa(unsigned char* key)
 {
 	int j = 0;
-	for (int i = 0; i < 256; i++) 
+	for (int i = 0; i < 256; i++)
 	{
 		j = (j + sbox[i] + key[i % sizeKey]) % 256;
 		swap(sbox, i, j);
 	}
 }
 
-void MyRc4::setKey(unsigned char k[], int size) 
+void MyRc4::setKey(unsigned char k[], int size)
 {
 	prgaIndexA = 0;
 	prgaIndexB = 0;
@@ -77,46 +79,34 @@ void MyRc4::setKey(unsigned char k[], int size)
 	ksa(k);
 }
 
-void rc4Par(unsigned char* key, int sizeOfKey, char* inputContent, char* outputTxt, int beginLimit, int endLimit) 
+void rc4Par(unsigned char* key, int sizeOfKey, char* inputContent, char* outputTxt, int beginLimit, int endLimit)
 {
 	MyRc4 rc4 = MyRc4();
 	rc4.setKey(key, sizeOfKey);
 	rc4.encrypt(inputContent, outputTxt, beginLimit, endLimit);
 }
 
-void useRc4(char* option, unsigned char* key, string inputFile, const char* outputFile, int nbThreads) 
+void useRc4(char* option, unsigned char* key, string inputFile, int nbThreads)
 {
 	clock_t begin = clock();
+	nbThreads = 1;
+	char* outputFile;
+	int sizeOfKey = strlen(reinterpret_cast<char*>(key));
+	int sizeOfInput = inputFile.length();
 
-	const int sizeOfKey = strlen(reinterpret_cast<char*>(key));
-	const int sizeOfInput = inputFile.length();
 	char* outputTxt = new char[sizeOfInput];
 
 	for (int i = 0; i < sizeOfInput; i++)
 	{
 		outputTxt[i] = ' ';
 	}
-	string optionInfo;
-
-	if (strcmp(option, "-e") == 0)
-	{
-		optionInfo = "encrypted";
-	}
-	else if (strcmp(option, "-d") == 0)
-	{
-		optionInfo = "decrypted";
-	}
-	else 
-	{
-		cout << "Option error... {-e|-d}" << endl;
-		return;
-	}	
+	string optionInfo = ((strcmp(option, "-e")) == 0) ? "encrypted" : "decrypted";
 
 	cout << "Your file is about to be " << optionInfo << endl;
 	char* inputTxt = const_cast<char*>(inputFile.c_str());
 	int beginLimit = 0, endLimit = 0;
 
-	for (int i = 0; i < nbThreads; i++) 
+	for (int i = 0; i < nbThreads; i++)
 	{
 		beginLimit = ((i == 0) ? 0 : (sizeOfInput / nbThreads) * i);
 		endLimit = ((i == nbThreads - 1) ? sizeOfInput : (sizeOfInput / nbThreads) * (i + 1));
@@ -124,46 +114,41 @@ void useRc4(char* option, unsigned char* key, string inputFile, const char* outp
 		myThread.join();
 	}
 
-	ofstream out(outputFile + '.' + optionInfo + ".txt");
+	ofstream out(outputFile);
 	out << outputTxt;
 	out.close();
 
-	cout << "Your file got " << optionInfo << endl;
-
 	clock_t end = clock();
 
-	double elapsed = end - begin;
-	cout << "Elapsed time = " << elapsed << endl;
+	cout << "Your file got " << optionInfo << endl;
+	cout << "Elapsed time = " << (double)end - begin << endl;
 }
 
-int main(int argc, char** argv) 
+
+/**
+ * mkdir build
+ * cd build
+ * cmake ..
+ * make
+ * */
+int main(int argc, char** argv)
 {
-	if(argc != 5) 
+	if (argc != 4)
 	{
-		cout << "This RC4 can be used by using these option" << endl;
-		cout << "main {-e|-d} NB_THREADS INPUT_FILE OUTPUT_FILE" << endl;
+		cout << "This RC4 can be used by using these option :" << endl;
+		cout << "main {-e|-d} NB_THREADS KEY INPUT_FILE" << endl;
 		return -1;
 	}
 
-	// get the option
 	char* option = argv[1];
-
-	// get nb thread
 	int nbOfThread = atoi(argv[2]);
-
-	// get input file name
-	const char* inputFilePath = reinterpret_cast<const char*>(argv[3]);
+	unsigned char* key = (unsigned char*)argv[3]; // (unsigned char*)"ESGI";
+	char* inputFilePath = (char*)(argv[4]); //reinterpret_cast<_>
 	ifstream ifs(inputFilePath);
-	const string inputFile((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
+	string inputFile((istreambuf_iterator<char>(ifs)), (istreambuf_iterator<char>()));
+	char* outputFile = argv[4];
 
-	// get output file name
-	const char* outputFile = argv[4];
-
-	// the constant key
-	unsigned char* key = (unsigned char*)"ESGI";
-
-	// rc4 is simetric, we just keep the option for the sake of it
-	useRc4(option, key, inputFile, outputFile, nbOfThread);
+	useRc4(option, key, inputFile, nbOfThread);
 
 	return 0;
 }
